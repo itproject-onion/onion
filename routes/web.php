f7e9695f8da99ece2c0e7e0f89f12be87b191231
cosimella <87551558+cosimella@users.noreply.github.com>
2026-04-25 21:40:02 +0200

    homepage mit konfigurator und platzhaltern für api einbindung

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,12 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
+
+use Illuminate\Http\Request;
+
+Route::post('/save-outfit', function (Request $request) {
+    return response()->json([
+        'message' => 'Outfit-IDs empfangen!',
+        'daten' => $request->all()
+    ]);
+})->name('outfit.save');
