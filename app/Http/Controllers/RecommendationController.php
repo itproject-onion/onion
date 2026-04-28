dd0e860d08e1ef71b5bf693c23281337c59558f9
Max <cmgyfx9wmw@privaterelay.appleid.com>
2026-04-28 10:19:23 +0200

    Umbenennung welcome.blade.php zu home.blade.php Verschieben von images-files in storage  (ausführen von "php artisan storage:link" notwendig!) Korrektur DB-Schema

--- app/Http/Controllers/RecommendationController.php
+++ app/Http/Controllers/RecommendationController.php
@@ -55,17 +55,17 @@ class RecommendationController extends Controller
             'head' => [
                 [
                     'id' => 1,
-                    'img' => '/images/konfig/head1_cosi.png',
+                    'img' => '/storage/head1_cosi.png',
                     'tags' => ['sun', 'summer']
                 ],
                 [
                     'id' => 2,
-                    'img' => '/images/konfig/head2_cosi.png',
+                    'img' => '/storage/head2_cosi.png',
                     'tags' => ['rain', 'cold']
                 ],
                 [
                     'id' => 3,
-                    'img' => '/images/konfig/head3_cosi.png',
+                    'img' => '/storage/head3_cosi.png',
                     'tags' => []
                 ],
             ],
@@ -73,22 +73,22 @@ class RecommendationController extends Controller
             'upper_shirt' => [
                 [
                     'id' => 4,
-                    'img' => '/images/konfig/shirt1_cosi.png',
+                    'img' => '/storage/shirt1_cosi.png',
                     'tags' => ['sun', 'light']
                 ],
                 [
                     'id' => 5,
-                    'img' => '/images/konfig/shirt2_cosi.png',
+                    'img' => '/storage/shirt2_cosi.png',
                     'tags' => ['cold', 'winter']
                 ],
                 [
                     'id' => 6,
-                    'img' => '/images/konfig/shirt3_cosi.png',
+                    'img' => '/storage/shirt3_cosi.png',
                     'tags' => ['casual']
                 ],
                 [
                     'id' => 19,
-                    'img' => '/images/konfig/shirt4_cosi.png',
+                    'img' => '/storage/shirt4_cosi.png',
                     'tags' => ['sport']
                 ],
             ],
@@ -96,17 +96,17 @@ class RecommendationController extends Controller
             'upper_pulli' => [
                 [
                     'id' => 7,
-                    'img' => '/images/konfig/pulli1_cosi.png',
+                    'img' => '/storage/pulli1_cosi.png',
                     'tags' => ['cold', 'winter']
                 ],
                 [
                     'id' => 8,
-                    'img' => '/images/konfig/pulli2_cosi.png',
+                    'img' => '/storage/pulli2_cosi.png',
                     'tags' => ['casual']
                 ],
                 [
                     'id' => 9,
-                    'img' => '/images/konfig/pulli3_cosi.png',
+                    'img' => '/storage/pulli3_cosi.png',
                     'tags' => ['sport']
                 ],
             ],
@@ -114,17 +114,17 @@ class RecommendationController extends Controller
             'upper_jacke' => [
                 [
                     'id' => 10,
-                    'img' => '/images/konfig/jacke1_cosi.png',
+                    'img' => '/storage/jacke1_cosi.png',
                     'tags' => ['rain', 'wind']
                 ],
                 [
                     'id' => 11,
-                    'img' => '/images/konfig/jacke2_cosi.png',
+                    'img' => '/storage/jacke2_cosi.png',
                     'tags' => ['cold', 'winter']
                 ],
                 [
                     'id' => 12,
-                    'img' => '/images/konfig/jacke3_cosi.png',
+                    'img' => '/storage/jacke3_cosi.png',
                     'tags' => ['light']
                 ],
             ],
@@ -132,17 +132,17 @@ class RecommendationController extends Controller
             'lower' => [
                 [
                     'id' => 13,
-                    'img' => '/images/konfig/lower1_cosi.png',
+                    'img' => '/storage/lower1_cosi.png',
                     'tags' => ['summer', 'light']
                 ],
                 [
                     'id' => 14,
-                    'img' => '/images/konfig/lower2_cosi.png',
+                    'img' => '/storage/lower2_cosi.png',
                     'tags' => ['casual']
                 ],
                 [
                     'id' => 15,
-                    'img' => '/images/konfig/lower3_cosi.png',
+                    'img' => '/storage/lower3_cosi.png',
                     'tags' => ['winter', 'cold']
                 ],
             ],
@@ -150,23 +150,23 @@ class RecommendationController extends Controller
             'feet' => [
                 [
                     'id' => 16,
-                    'img' => '/images/konfig/feet1_cosi.png',
+                    'img' => '/storage/feet1_cosi.png',
                     'tags' => ['sport']
                 ],
                 [
                     'id' => 17,
-                    'img' => '/images/konfig/feet2_cosi.png',
+                    'img' => '/storage/feet2_cosi.png',
                     'tags' => ['rain']
                 ],
                 [
                     'id' => 18,
-                    'img' => '/images/konfig/feet3_cosi.png',
+                    'img' => '/storage/feet3_cosi.png',
                     'tags' => ['casual', 'summer']
                 ],
             ],
         ];
 
-        return view('welcome', [
+        return view('home', [
             'recommendations' => $recommendations,
             'tags' => $tags,
             'categories' => $categories,
