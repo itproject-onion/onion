3f209df57416296c47539e34d48623d9545278b3
shu-leonie <leonie.rank@arcor.de>
2026-05-11 13:29:34 +0000

    Implement CategorySeeder

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class CategorySeeder extends Seeder
@@ -12,5 +13,23 @@ class CategorySeeder extends Seeder
     public function run(): void
     {
         // create categories
+
+        $categories = [
+            ['name' => 'Kopfbedeckung', 'is_impacted_by_rain' => true],
+            ['name' => 'T-Shirt', 'is_impacted_by_rain' => false],
+            ['name' => 'Pullover', 'is_impacted_by_rain' => false],
+            ['name' => 'Jacke', 'is_impacted_by_rain' => true],
+            ['name' => 'Hose', 'is_impacted_by_rain' => true],
+            ['name' => 'Strumpfhose', 'is_impacted_by_rain' => false],
+            ['name' => 'Socken', 'is_impacted_by_rain' => false],
+            ['name' => 'Schuhe', 'is_impacted_by_rain' => true],
+            ['name' => 'Accessoires', 'is_impacted_by_rain' => true],
+            ['name' => 'Sonnenbrille', 'is_impacted_by_rain' => false],
+            ['name' => 'Sonnencreme', 'is_impacted_by_rain' => false],
+        ];
+
+        foreach ($categories as $category) {
+            Category::updateOrCreate(['name' => $category['name']], $category);
+        }
     }
 }
